953c07f5057970c2ab539e2418b580279f9edff7
mohmmal ali <mohmmal ali@DESKTOP-6KABRDI>
2023-09-27 14:19:26 +0300

    testing

---
+++ project_DataStructures/src/Contact.java
@@ -0,0 +1,81 @@
+
+public class Contact implements Comparable<Contact>{
+	
+	private String name ;
+	private String phoneNumber ;
+	private String emailAddress;
+	private String address ;
+	private String birthday ;
+	private String notes ;
+	
+	public Contact(String name, String phoneNumber, String emailAddress, String address, String birthday,String notes){
+
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.emailAddress = emailAddress;
+		this.address = address;
+		this.birthday = birthday;
+		this.notes = notes;
+	}
+	
+	public Contact() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	
+	 @Override
+	    public int compareTo(Contact otherContact) {
+	        return this.name.compareTo(otherContact.name);
+	    }
+	
+
+}
