ded89237b56214af1994d901158b0ef0231d90de
mohmmadalz <140457479+mohmmadalz@users.noreply.github.com>
2023-10-02 15:17:58 +0300

    Update LinkedList.java
    new implementation for class LinkedList

--- project_DataStructures/src/LinkedList.java
+++ project_DataStructures/src/LinkedList.java
@@ -1,95 +1,81 @@
-public class LinkedList<T extends Comparable<T>> {
-    private Node<T> head;
-    
-    // Inner Node class to represent elements in the linked list
-    private static class Node<T> {
-        T data;
-        Node<T> next;
-        
-        //Making double LinkedList
-        
-        Node(T data) {
-            this.data = data;
-            this.next = null;
-            
-
-        }
-        
-        
-    }
-    
-    // Constructor to initialize an empty linked list
-    public LinkedList() {
-        head = null;
-    }
-    
-    // Method to add a new element to the linked list
-    public void add(T data) {
-        Node<T> newNode = new Node<>(data);
-        if (head == null) {
-            head = newNode;
-        } else {
-            Node<T> current = head;
-            while (current.next != null) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-    
-    // Method to search for an element in the linked list
-    public T search(T key) {
-        Node<T> current = head;
-        while (current != null) {
-            if (current.data.equals(key)) {
-                return current.data;
-            }
-            current = current.next;
-        }
-        return null; // Not found
-    }
-    
-    // Method to delete an element from the linked list
-    public void delete(T key) {
-        if (head == null) {
-            return; // Empty list
-        }
-        if (head.data.equals(key)) {
-            head = head.next;
-            return;
-        }
-        Node<T> current = head;
-        while (current.next != null) {
-            if (current.next.data.equals(key)) {
-                current.next = current.next.next;
-                return;
-            }
-            current = current.next;
-        }
-    }
-    
-    // Method to print all elements in the linked list
-    public void printAll() {
-        Node<T> current = head;
-        while (current != null) {
-            System.out.println(current.data);
-            current = current.next;
-        }
-    }
-    
-    // Method to check if the linked list is empty
-    public boolean isEmpty() {
-        return head == null;
-    }
-    
-    // Method to get the size of the linked list
-    public int size() {
-        int count = 0;
-        Node<T> current = head;
-        while (current != null) {
-            count++;
-            current = current.next;
-        }
-        return count;
-    }
-}
+   public class LinkedList<T> implements List<T> {
+	// for contact
+	    private Node<T> head;
+	    private Node<T> current;
+	   
+	   
+	   public LinkedList() {
+	       head =current = null;
+	   }
+	   
+	   public boolean empty() {
+	   	return head==null;
+	   }
+	   
+	   public boolean last() {
+	   	return current.next == null ;
+	   }
+	   
+	   public boolean full() {
+	   	return false ;
+	   }
+	   
+	   public void findFirst() {
+	   	current = head;
+	   }
+	   
+	  public void findNext() {
+	   	current = current.next;
+	   }
+	  
+	  public T retrieve() {
+		   return current.data;
+	  }
+	  
+	  public void update(T val) {
+		   current.data=val;
+	  }
+	  
+	  public void insert(T val) {
+		   Node<T> tmp;
+		   Node<T> NewNode = new Node<T> (val) ;
+		   
+				if (empty()) {
+					current = head = NewNode;
+				}
+				else {
+					tmp = current.next;
+					current.next = new Node<T> (val);
+					current = current.next;
+					current.next = tmp;
+				}
+		}
+	
+	  
+	  public void remove() {
+		   
+			   
+			   if (current == head) {
+					head = head.next;
+				}
+				else {
+					Node<T> tmp = head;
+			
+					while (tmp.next != current)
+						tmp = tmp.next;
+			
+					tmp.next = current.next;
+				}
+			
+				if (current.next == null)
+					current = head;
+				else
+					current = current.next;
+			}
+	
+	   
+	   
+	   
+	  
+	  
+	}
