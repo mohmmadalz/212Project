12e7e5ea1f85883d880006cc57e2af1babe91ac3
mohmmadalz <140457479+mohmmadalz@users.noreply.github.com>
2023-10-04 18:59:29 +0300

    Update Event.java
    small change

--- src/project_DataStructures/src/Event.java
+++ src/project_DataStructures/src/Event.java
@@ -1,4 +1,33 @@
-
 public class Event {
 //hi Mo is here
+    private String title;
+    private Date date;
+    private String location;
+    private Contact contact;
+
+    public Event(String title, String date, String location, Contact contact) {
+        this.title = title;
+        this.location = location;
+        this.contact = contact;
+        this.date = date;
+       
+    }
+
+    public String getTitle() {
+          return title;
+      }
+  
+      public Date getDate() {
+          return date;
+      }
+  
+      public String getLocation() {
+          return location;
+      }
+  
+      public Contact getContact() {
+          return contact;
+      }
+
+      
 }
