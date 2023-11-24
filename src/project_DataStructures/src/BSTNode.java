405e4cb106652266d0d9b076a01c731823639aaf
mohmmed alz <140457479+mohmmadalz@users.noreply.github.com>
2023-11-25 01:01:35 +0300

    Update BSTNode.java

--- src/project_DataStructures/src/BSTNode.java
+++ src/project_DataStructures/src/BSTNode.java
@@ -1,23 +1,67 @@
 	
 public class BSTNode <T> {
 	
-			public int key;
+			public String key;
 			public T data;
 			public BSTNode<T> left, right;
 			
-			/** Creates a new instance of BSTNode */
-			public BSTNode(int k, T val) {
+			// Creates a new instance of BSTNode 
+			
+			public BSTNode(String k, T val) {      // without left and right
 				key = k;
 				data = val;
 				left = right = null;
 			}
 			
-			public BSTNode(int k, T val, BSTNode<T> l, BSTNode<T> r) {
+			public BSTNode(String k, T val, BSTNode<T> l, BSTNode<T> r) {     // with left and right
 				key = k;
 				data = val;
 				left = l;
 				right = r;
 			}
+
+
+			public void setKey(String key2) {
+				this.key = key2;
+			}
+
+			
+
+			public void setData(T data2) {
+				this.data = data2 ;
+			}
+			
+			
+
+			public void setLeft(BSTNode<T> p) {
+				this.left = p ;
+			}
+
+			public void setRight(BSTNode<T> p) {
+				this.right = p ;
+			}
+			
+			
+			public String getKey() {
+				
+				return key ;
+			}
+			
+			public T getData() {
+				
+				return data;
+			}
+
+			public BSTNode<T> getLeft() {
+				
+				return left;
+			}
+
+			public BSTNode<T> getRight() {
+				
+				return right;
+			}
+			
 			
 		}
 	
