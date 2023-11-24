19a67c31f76694802f53f24fd7ea204d8ac26917
mohmmed alz <140457479+mohmmadalz@users.noreply.github.com>
2023-11-25 01:03:55 +0300

    Update Event.java

--- src/project_DataStructures/src/Event.java
+++ src/project_DataStructures/src/Event.java
@@ -1,67 +1,70 @@
+
 public class Event implements Comparable<Event> {
-	private String title;
-	private String date;
-	private String location;
-	private Contact eventuser;
-	// add appointment
+		private String title;
+		private String date;
+		private String location;
+		private Contact eventuser;
+		// add an aappointment 
+		// modify the class as requested
+
+		public Event(String title, String date, String location, Contact contact) {
+			this.title = title;
+			this.location = location;
+			this.eventuser = contact;
+			this.date = date;
 
 
-	public Event(String title, String date, String location, Contact contact) {
-		this.title = title;
-		this.location = location;
-		this.eventuser = contact;
-		this.date = date;
+		}
+
+		public Event() {
+			
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public String getDate() {
+			return date;
+		}
+
+		public String getLocation() {
+			return location;
+		}
+		public Contact getContact() {
+			return eventuser;
+		}
+		public Contact getEventuser() {
+			return eventuser;
+		}
+
+		public void setEventuser(Contact eventuser) {
+			this.eventuser = eventuser;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+		public void setDate(String date) {
+			this.date = date;
+		}
+
+		public void setLocation(String location) {
+			this.location = location;
+		}
 
 
+		@Override
+	    public int compareTo(Event otherEvent) {
+	        return this.title.compareTo(otherEvent.title);
+	    }
+		public String Info() {
+			return("\nEvent title: " + getTitle()+
+			"\nContact name: " + eventuser.getName()+            
+			"\nEvent date and time (MM/DD/YYYY HH:MM):  " + date +
+			"\nEvent location: " + location+"\n");
+		}
 	}
 
-	public Event() {
-		// TODO Auto-generated constructor stub
-	}
 
-	public String getTitle() {
-		return title;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public String getLocation() {
-		return location;
-	}
-	public Contact getContact() {
-		return eventuser;
-	}
-	public Contact getEventuser() {
-		return eventuser;
-	}
-
-	public void setEventuser(Contact eventuser) {
-		this.eventuser = eventuser;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-
-
-	@Override
-	public int compareTo(Event otherEvent) {
-		return this.title.compareTo(otherEvent.title);
-	}
-	public String Info() {
-		return("\nEvent title: " + getTitle()+
-				"\nContact name: " + eventuser.getName()+
-				"\nEvent date and time (MM/DD/YYYY HH:MM):  " + date +
-				"\nEvent location: " + location+"\n");
-	}
-}
