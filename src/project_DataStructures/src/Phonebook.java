f159bcee7d03608d1ed056a4637ce0d5d4bc1edf
mohmmed alz <mohmmadalz1424@gmail.com>
2024-03-04 23:56:46 +0300

    final changes

--- src/project_DataStructures/src/Phonebook.java
+++ src/project_DataStructures/src/Phonebook.java
@@ -1,26 +1,4 @@
-//*************Example***************
-//
-//CLASS: Phonebook.java
-//
-//CSC212 Data structures - Project phase II
-//
-//Fall 2023
-//
-//EDIT DATE: 3/12/2023
-//
-//
-//
-//TEAM: faiasl && mohmmed && mohmmed
-//
-//Team Name : Null Byet
-//
-//AUTHORS:
-//
-//443101660 faisal alhaqbani
-//443100700 mohmmed alzobudi
-//443101706 mohmmed almuhaitheef
-//
-////***********************************
+
 import java.util.Scanner;
 
 public class Phonebook {
